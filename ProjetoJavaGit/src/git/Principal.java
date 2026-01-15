4e8b10152db1bda250fc7941c051a49845e128cf
Emerson <cardosoemerson501@gmail.com>
2026-01-15 11:57:09 +0200

    terceiro commitr

--- ProjetoJavaGit/src/git/Principal.java
+++ ProjetoJavaGit/src/git/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Projeto Java ligado ao GitHub");
+		 int num=12;
+		 System.out.println("opem "+num);
 	}
 
 }
