426e8cc7ae4a24da40676dd44df5572189bc9eba
Emerson <cardosoemerson501@gmail.com>
2026-01-15 11:53:59 +0200

    segundo

---
+++ ProjetoJavaGit/src/git/Principal.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Projeto Java ligado ao GitHub");
+	}
+
+}
